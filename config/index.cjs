6ce2c885c6da0697f393b8c2175c58e7b40f2e1d
linjie1212 <112303600+linjie1212@users.noreply.github.com>
2023-05-22 09:20:54 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: '21uuo568k1fVOd31z6j7TVpLT4tlGE0VxFfqEPNPWXI',
 
   CALLBACK_USERS: [
     {
